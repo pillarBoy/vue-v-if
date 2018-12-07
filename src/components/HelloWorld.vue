37eaa4961f829f70af5f4d07d3f919bfa1ffb283
pillarBoy <601991736@qq.com>
2018-12-07 17:34:52 +0800

    add files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <div v-if="msg=='email'">email</div>
+    <p v-else>phone</p>
+    <button @click="change">change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'email'
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.msg = 'email'
+    }, 2000)
+  },
+  methods: {
+    change() {
+      this.msg = this.msg==='email' ? 'phone' : 'email';
+      console.log(this.msg);
+    }
+  }
+
+}
+</script>
